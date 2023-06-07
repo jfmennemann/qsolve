daa46f1fa0c9ad05a90f52d4124579c789f4dbfe
jfmennemann <jfmennemann@gmx.de>
2023-06-07 16:12:08 +0200

    minor modification

--- examples/examples_1d/harmonic_trap_1d/main_harmonic_trap_1d.py
+++ examples/examples_1d/harmonic_trap_1d/main_harmonic_trap_1d.py
@@ -186,18 +186,30 @@ solver.set_external_potential(t=0.0, u=u_of_times[0])
 
 import torch
 
-A = torch.tensor([[12., -51, 4], [6, 167, -68], [-4, 24, -41]])
+A = torch.tensor([[12., -51, 4], [6, 167, -68], [-4, 24, -41]], dtype=torch.complex128)
 
-print(A.round())
+# print(A.round())
 
 Q, R = torch.linalg.qr(A)
 
-print(Q.round())
-print(R.round())
+# print(Q.round())
+# print(R.round())
 
 Q_times_R = Q @ R
 
-print(Q_times_R.round())
+# print(Q_times_R.round())
+
+c_00 = torch.sum(torch.conj(Q[:, 0]) * Q[:, 0])
+c_01 = torch.sum(torch.conj(Q[:, 0]) * Q[:, 1])
+
+print(c_00.cpu().numpy())
+print(c_01.cpu().numpy())
+
+tmp = torch.adjoint(Q) @ Q
+
+tmp = tmp.cpu().numpy()
+
+print(tmp.round())
 
 input()
 
