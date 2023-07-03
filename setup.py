11b137f148f852e67abb0b840d256f7e4fc7b917
jfmennemann <jfmennemann@gmx.de>
2023-07-03 14:03:40 +0200

    minor update

--- setup.py
+++ setup.py
@@ -4,30 +4,21 @@ from setuptools import setup, find_packages
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
-    
     long_description = fh.read()
 
 
 setuptools.setup(
     name="qsolve",
-    version="0.3.1",
+    version="0.3.2",
     url = "https://github.com/jfmennemann/qsolve",
     author="Jan-Frederik Mennemann",
     author_email="jfmennemann@gmx.de",
     description="Numerical framework for the simulation and optimization of ultracold atom experiments",
-    # long_description=read('README.md'),
     long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[]
-    # classifiers=[
-    #     'Programming Language :: Python',
-    #     'Programming Language :: Python :: 3',
-    #     'Programming Language :: Python :: 3.10',
-    # ],
-    # package_data={'': ['/qsolve/qsolve/core/*.pyc']},
-    # license="MIT",
-    # keywords="ultracold atoms, classical fields simulations, Gross-Pitaevskii equation, thermal state sampling, time of flight"
 )
 
