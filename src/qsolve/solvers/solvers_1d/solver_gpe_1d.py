4f0601f203879850df10cffcff37d6a244fd6840
jfmennemann <jfmennemann@gmx.de>
2023-08-01 16:07:55 +0200

    bdg_1d, sparse matrix eigenvalue problem

--- src/qsolve/solvers/solvers_1d/solver_gpe_1d.py
+++ src/qsolve/solvers/solvers_1d/solver_gpe_1d.py
@@ -236,7 +236,7 @@ class SolverGPE1D(object):
         # _psi_0 = torch.tensor(psi_0 / self._units.unit_wave_function, device=self._device)
         # _mue_0 = mue_0 / self._units.unit_energy
 
-        _eigenvectors_u, _eigenvectors_v, _eigenvalues_omega, _psi_0, _mue_0 = qsolve_core.bdg_1d(
+        _eigenvectors_u, _eigenvectors_v, _eigenvalues_omega, _psi_0, _mue_0 = qsolve_core.bdg_1d_sparse(
             self._V,
             self._dx,
             self._hbar,
@@ -246,11 +246,18 @@ class SolverGPE1D(object):
             n
         )
 
+        # return \
+        #     self._units.unit_wave_function * _eigenvectors_u.cpu().numpy(), \
+        #     self._units.unit_wave_function * _eigenvectors_v.cpu().numpy(), \
+        #     self._units.unit_frequency * _eigenvalues_omega.cpu().numpy(), \
+        #     self._units.unit_wave_function * _psi_0.cpu().numpy(), \
+        #     self._units.unit_energy * _mue_0
+
         return \
-            self._units.unit_wave_function * _eigenvectors_u.cpu().numpy(), \
-            self._units.unit_wave_function * _eigenvectors_v.cpu().numpy(), \
-            self._units.unit_frequency * _eigenvalues_omega.cpu().numpy(), \
-            self._units.unit_wave_function * _psi_0.cpu().numpy(), \
+            self._units.unit_wave_function * _eigenvectors_u, \
+            self._units.unit_wave_function * _eigenvectors_v, \
+            self._units.unit_frequency * _eigenvalues_omega, \
+            self._units.unit_wave_function * _psi_0, \
             self._units.unit_energy * _mue_0
 
     def init_sgpe(self, **kwargs):
